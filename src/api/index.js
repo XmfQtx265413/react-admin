76a7da2a30d076a8113de4e69f6164518c7bd7e3
xiatian <1951774463@qq.com>
2019-06-24 20:24:50 +0800

    '退出、时间、天气'

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,5 @@
+import jsonp from 'jsonp';
+import { message } from "antd";
 import ajax from './ajax';
 
 // export const reqLogin = (data) => ajax('/login', data, 'POST');
@@ -19,3 +21,23 @@ export const reqLogin = (username, password) => ajax('/login', {username, passwo
  */
 
 export const reqValidateUserInfo = (id) => ajax('/validate/user', {id} , 'POST');
+//获取异步代码的返回值，必须用promise
+//避免一开始就加载，调用才调用，外层用一个函数包裹
+export const reqWeather = function () {
+  return new Promise((resolve, reject) => {
+    jsonp(`http://api.map.baidu.com/telematics/v3/weather?location=深圳&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`, {}, function (err, data) {
+      if (!err) {
+        const { dayPictureUrl, weather} = data.results[0].weather_data[0];
+        resolve({
+          weatherImg: dayPictureUrl,
+          weather
+        }) ;
+      } else {
+        message.error('请求失败，请刷新重试');
+        resolve();
+      }
+    });
+  });
+
+};
+
--- src/components/header-main/index.jsx
+++ src/components/header-main/index.jsx
@@ -1,25 +1,72 @@
 import React, { Component } from 'react';
+import { Modal } from 'antd';
+import { withRouter} from 'react-router-dom';
+import dayjs from 'dayjs';
 
 import MyButton from '../my-button';
+import { getItem, removeItem } from "../../utils/storage-tools";
+import { reqWeather } from '../../api';
 
 import logo from '../../assets/images/logo.png';
 import './index.less';
 
-export default class HeaderMain extends Component {
+class HeaderMain extends Component {
+  state = {
+    sysTime: Date.now(),
+    weather: '晴',
+    weatherImg: 'http://api.map.baidu.com/images/weather/day/qing.png'
+  };
+
+  componentWillMount() {
+    //只需要读取一次
+    this.username = getItem().username
+  }
+
+  async componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        sysTime: Date.now()
+      })
+    }, 1000);
+    //发送请求，请求天气
+    const result = await reqWeather()
+    if (result) {
+      this.setState(result);
+    }
+  }
+
+  logout = () => {
+    Modal.confirm({
+      title: '你确认要退出登陆吗',
+      okText: '确认',
+      cancelText: '取消',
+      onOk: () => {
+        //清空本地数据
+        removeItem();
+        //推出登录
+        //先清空再切换
+        this.props.history.replace('/login')
+      },
+    })
+  };
+
   render() {
+    const { sysTime, weather, weatherImg } = this.state;
     return <div>
       <div className="header-main-top">
-        <span>欢迎, admin</span>
-        <MyButton>退出</MyButton>
+        <span>欢迎, {this.username}</span>
+        <MyButton onClick={this.logout}>退出</MyButton>
       </div>
       <div className="header-main-bottom">
         <span className="header-main-left">用户管理</span>
         <div className="header-main-right">
-          <span>{Date.now()}</span>
-          <img src={logo} alt=""/>
-          <span>晴</span>
+          <span>{dayjs(sysTime).format('YYYY-MM-DD HH:mm:ss')}</span>
+          <img src={weatherImg} alt=""/>
+          <span>{weather}</span>
         </div>
       </div>
     </div>;
   }
 }
+
+export default withRouter(HeaderMain)
